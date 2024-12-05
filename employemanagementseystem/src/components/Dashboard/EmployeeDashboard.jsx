5fc38aeb03d89db1544e2e3fe99b601a50438b65
shailkumari1 <shailykatiyar36@gmail.com>
2024-12-05 20:12:38 +0530

    employe dashboard

--- employemanagementseystem/src/components/Dashboard/EmployeeDashboard.jsx
+++ employemanagementseystem/src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Header from '../Others/Header'
 import TasklistNumbers from '../Others/TasklistNumbers'
+import TaskList from '../Tasklist/TaskList'
 
 const EmployeeDashboard = () => {
   return (
     <div className='p-5 bg-[#1c1c1c] h-screen'>
       <Header/>
       <TasklistNumbers/>
-      
+      <TaskList/>
  
     </div>
   )
---
+++ employemanagementseystem/src/components/Tasklist/TaskList.jsx
@@ -0,0 +1,153 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div id='Tasklist' className='h-[55%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5 mt-5'>
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-yellow-500 rounded-2xl'>
+            <div className='flex justify-between items-center'>
+               <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+               <h4 className='text-sm'>05-Dec-2024</h4>
+                 
+            </div>
+
+            <h2 className='font-semibold text-2xl mt-5'>Make a new project </h2>
+            <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam soluta, nemo magni quas, laboriosam laudantium incidunt perspiciatis aliquam iusto placeat, quis molestias unde quod sunt.</p>
+
+        </div>
+
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-yellow-500 rounded-2xl'>
+            <div className='flex justify-between items-center'>
+               <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+               <h4 className='text-sm'>05-Dec-2024</h4>
+                 
+            </div>
+
+            <h2 className='font-semibold text-2xl mt-5'>Make a new project </h2>
+            <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam soluta, nemo magni quas, laboriosam laudantium incidunt perspiciatis aliquam iusto placeat, quis molestias unde quod sunt.</p>
+
+        </div>
+
+
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-yellow-500 rounded-2xl'>
+            <div className='flex justify-between items-center'>
+               <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+               <h4 className='text-sm'>05-Dec-2024</h4>
+                 
+            </div>
+
+            <h2 className='font-semibold text-2xl mt-5'>Make a new project </h2>
+            <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam soluta, nemo magni quas, laboriosam laudantium incidunt perspiciatis aliquam iusto placeat, quis molestias unde quod sunt.</p>
+
+        </div>
+
+
+
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-yellow-500 rounded-2xl'>
+            <div className='flex justify-between items-center'>
+               <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+               <h4 className='text-sm'>05-Dec-2024</h4>
+                 
+            </div>
+
+            <h2 className='font-semibold text-2xl mt-5'>Make a new project </h2>
+            <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam soluta, nemo magni quas, laboriosam laudantium incidunt perspiciatis aliquam iusto placeat, quis molestias unde quod sunt.</p>
+
+        </div>
+
+
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-yellow-500 rounded-2xl'>
+            <div className='flex justify-between items-center'>
+               <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+               <h4 className='text-sm'>05-Dec-2024</h4>
+                 
+            </div>
+
+            <h2 className='font-semibold text-2xl mt-5'>Make a new project </h2>
+            <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam soluta, nemo magni quas, laboriosam laudantium incidunt perspiciatis aliquam iusto placeat, quis molestias unde quod sunt.</p>
+
+        </div>
+
+
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-yellow-500 rounded-2xl'>
+            <div className='flex justify-between items-center'>
+               <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+               <h4 className='text-sm'>05-Dec-2024</h4>
+                 
+            </div>
+
+            <h2 className='font-semibold text-2xl mt-5'>Make a new project </h2>
+            <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam soluta, nemo magni quas, laboriosam laudantium incidunt perspiciatis aliquam iusto placeat, quis molestias unde quod sunt.</p>
+
+        </div>
+
+
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-yellow-500 rounded-2xl'>
+            <div className='flex justify-between items-center'>
+               <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+               <h4 className='text-sm'>05-Dec-2024</h4>
+                 
+            </div>
+
+            <h2 className='font-semibold text-2xl mt-5'>Make a new project </h2>
+            <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam soluta, nemo magni quas, laboriosam laudantium incidunt perspiciatis aliquam iusto placeat, quis molestias unde quod sunt.</p>
+
+        </div>
+
+
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-yellow-500 rounded-2xl'>
+            <div className='flex justify-between items-center'>
+               <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+               <h4 className='text-sm'>05-Dec-2024</h4>
+                 
+            </div>
+
+            <h2 className='font-semibold text-2xl mt-5'>Make a new project </h2>
+            <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam soluta, nemo magni quas, laboriosam laudantium incidunt perspiciatis aliquam iusto placeat, quis molestias unde quod sunt.</p>
+
+        </div>
+
+
+
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-yellow-500 rounded-2xl'>
+            <div className='flex justify-between items-center'>
+               <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+               <h4 className='text-sm'>05-Dec-2024</h4>
+                 
+            </div>
+
+            <h2 className='font-semibold text-2xl mt-5'>Make a new project </h2>
+            <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam soluta, nemo magni quas, laboriosam laudantium incidunt perspiciatis aliquam iusto placeat, quis molestias unde quod sunt.</p>
+
+        </div>
+
+
+
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-yellow-500 rounded-2xl'>
+            <div className='flex justify-between items-center'>
+               <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+               <h4 className='text-sm'>05-Dec-2024</h4>
+                 
+            </div>
+
+            <h2 className='font-semibold text-2xl mt-5'>Make a new project </h2>
+            <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam soluta, nemo magni quas, laboriosam laudantium incidunt perspiciatis aliquam iusto placeat, quis molestias unde quod sunt.</p>
+
+        </div>
+
+
+        <div className='flex-shrink-0 h-full w-[300px] p-5 bg-yellow-500 rounded-2xl'>
+            <div className='flex justify-between items-center'>
+               <h3 className='bg-red-600 px-3 py-1 rounded text-sm'>High</h3>
+               <h4 className='text-sm'>05-Dec-2024</h4>
+                 
+            </div>
+
+            <h2 className='font-semibold text-2xl mt-5'>Make a new project </h2>
+            <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam soluta, nemo magni quas, laboriosam laudantium incidunt perspiciatis aliquam iusto placeat, quis molestias unde quod sunt.</p>
+
+        </div>
+    </div>
+  )
+}
+
+export default TaskList
